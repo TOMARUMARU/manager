4e145ce01c474baeda552785874a2576351dacbe
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-28 19:47:51 +0900

    import firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCPK19NEDjoEOlfTtPp9FOcpmZC39wv0BY',
+      authDomain: 'manager-37ff2.firebaseapp.com',
+      databaseURL: 'https://manager-37ff2.firebaseio.com',
+      projectId: 'manager-37ff2',
+      storageBucket: 'manager-37ff2.appspot.com',
+      messagingSenderId: '613031742312'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
