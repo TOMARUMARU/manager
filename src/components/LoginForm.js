ab880b128b795d6757f749b36c5b867458d299ba
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-29 15:44:47 +0900

    add LoginUser func

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
@@ -11,6 +11,13 @@ class LoginForm extends Component {
   onPasswordChange(text) {
     this.props.passwordChanged(text);
   }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+
+    this.props.loginUser({ email, password }); 
+  }
+
 
   render() {
     return (
@@ -35,9 +42,9 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button
+          <Button onPress={this.ButtonPress.bind(this)}>
             Login
-          />
+          </Button>
         </CardSection>
       </Card>
     );
@@ -51,7 +58,9 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, {
+   emailChanged, passwordChanged, loginUser
+})(LoginForm);
 
 // const mapDispatchToProps = (dispatch) => {
 //   return {
