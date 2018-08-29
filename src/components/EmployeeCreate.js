7ee4b59076c438504ea7dc2535c8d7ec4bed797d
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-29 22:07:11 +0900

    import Picker

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Picker } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -23,6 +24,18 @@ class EmployeeCreate extends Component {
             value={this.props.phone}
             onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
           />
+        </CardSection>
+
+        <CardSection>
+          <Picker>
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesdat" value="Wednesdat" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Fryday" value="Fryday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
         </CardSection>
 
         <CardSection>
