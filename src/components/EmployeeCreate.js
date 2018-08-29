f64edc9eb4d2341df21c0dc80c56a3fe9e63b9e6
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-29 21:46:23 +0900

    add employeeupdate connect

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
@@ -9,6 +11,7 @@ class EmployeeCreate extends Component {
           <Input
             label="Name"
             placeholder="都丸"
+            value={this.props.name}
           />
         </CardSection>
 
@@ -16,6 +19,7 @@ class EmployeeCreate extends Component {
           <Input
             label="Phone"
             placeholder="555-555-5555"
+            value={this.props.phone}
           />
         </CardSection>
 
@@ -29,4 +33,10 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = ({ employeeForm }) => {
+  const { name, phone, shift } = employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(null, { employeeUpdate } )(EmployeeCreate);
