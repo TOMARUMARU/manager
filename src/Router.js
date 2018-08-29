d950ba3a6072bdb8e5095dea653ea0f5d16ebb9d
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-29 19:58:53 +0900

    add parent scene

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
-import EmployeeList from './components/EmployeeList'
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ marginTop: 60 }}>
       <Scene key="root">
-        <Scene key="login" component={LoginForm} title="Please login" initial />
-        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+        <Scene key="auth">
+          <Scene key="login" component={LoginForm} title="Please login" initial />
+        </Scene>
+        <Scene key="main">
+          <Scene key="employeeList" component={EmployeeList} title="Employees" />
+        </Scene>
       </Scene>
     </Router>
   );
