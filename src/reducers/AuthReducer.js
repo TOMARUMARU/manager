732e8c8776435e21308e72967b00cb0ee0dc13c8
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-29 15:40:45 +0900

    fix

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -3,9 +3,14 @@ import {
    PASSWORD_CHANGED
 } from '../actions/types';
 
-const INITIAL_STATE = { email: '', password: '' };
+const INITIAL_STATE = {
+  email: '',
+  password: ''
+ };
 
 export default (state = INITIAL_STATE, action) => {
+  console.log(action);
+
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload };
