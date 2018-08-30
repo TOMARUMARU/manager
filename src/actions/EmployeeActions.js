87489018fad1e0ffbd87b7c15a6d2e220ad66719
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-30 13:26:33 +0900

    firebase push data

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -10,5 +10,6 @@ export const employeeUpdate = ({ prop, value }) => {
 
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
-  firebase.database().ref(`/users/${currentUser.uid}/employees`);
+  firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift });
 };
