71212a2d2dda20501590b864a08ba05643647b88
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-30 13:43:31 +0900

    case EMPLOYEE_CREATE

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,9 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE
+} from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
