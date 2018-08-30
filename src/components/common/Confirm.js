6e02726755011d82c829ba1d71fb0533cf568ce3
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-30 19:57:29 +0900

    create Confirm component

---
+++ src/components/common/Confirm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, VIew, Modal } from 'react-native';
+import { CardSection } from './CardSection';
+import { Button } from './Button';
+
+const Confirm = () => {
+
+};
+
+export { Confirm };
